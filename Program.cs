25b2d657967fb92daac0914f4436152ca525f5cd
Sebastián Gómez Morales <sebastian61910.sg@gmail.com>
2021-02-10 10:33:57 -0700

    Se implementa el FizzBuzz al proyecto.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numeros = new int[100];
+            
+            int i; 
+
+            for (i = 0; i < 100; i++) 
+            {
+                numeros[i] = i + 1;
+            }
+            
+            for (i = 0; i < numeros.Length; i++) 
+            {
+                if (numeros[i]%3 == 0 && numeros[i]%5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (numeros[i]%3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (numeros[i]%5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else 
+                Console.WriteLine(numeros[i]);
+            }
         }
     }
 }
